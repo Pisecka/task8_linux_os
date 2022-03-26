d0a6be11f2e4fdc5371a656612e2672f5f857724
Karina <pisecka.karina@gmail.com>
2022-03-26 09:42:28 +0200

    print in console and remove null processess

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import subprocess
 import pandas as pd
 
-#
+# capture output from command "ps aux"
 res = subprocess.run('ps aux', shell=True, capture_output=True, text=True).stdout
 
 # split text in rows by delimeter "/n"
@@ -13,6 +13,8 @@ list2 = [i.split(maxsplit=10) for i in list1]
 # create pandas DataFrame as we have tabular data
 df = pd.DataFrame(list2[1:], columns=list2[0])
 
+# remove None processes
+df = df[df.USER.notna()]
 
 # Пользователи системы: 'root', 'user1', ...
 users = list(df.USER.unique())
@@ -29,16 +31,15 @@ user_proccesses = df.USER.value_counts().reset_index().rename(columns={'index': 
 df['%MEM'] = df['%MEM'].astype(float)
 memory_usage = df['%MEM'].sum()
 
-
 # Всего CPU используется: 33.2%
 df['%CPU'] = df['%CPU'].astype(float)
 cpu_usage = df['%CPU'].sum()
 
 # Больше всего памяти использует: (%имя процесса, первые 20 символов если оно длиннее)
-max_memory_usage = df[df['%MEM']==df['%MEM'].max()].iloc[0, 10][:20]
+max_memory_usage = df[df['%MEM'] == df['%MEM'].max()].iloc[0, 10][:20]
 
 # Больше всего CPU использует: (%имя процесса, первые 20 символов если оно длиннее)
-max_cpu_usage = df[df['%CPU']==df['%CPU'].max()].iloc[0, 10][:20]
+max_cpu_usage = df[df['%CPU'] == df['%CPU'].max()].iloc[0, 10][:20]
 
 template = f'''
 Отчёт о состоянии системы:
@@ -53,5 +54,9 @@ template = f'''
 Больше всего CPU использует: {max_cpu_usage}
 '''
 
+# save in file: report.txt
 with open('report.txt', 'w') as f:
     print(template, file=f)
+
+# print in console
+print(template)
